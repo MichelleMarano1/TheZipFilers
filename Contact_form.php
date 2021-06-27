5ca604121bea23b5d0fc8d2c30f23285f0e1071f
MichelleMarano <72081450+MichelleMarano@users.noreply.github.com>
2021-06-27 16:22:01 -0400

    m

--- Contact_form.php
+++ Contact_form.php
@@ -1,21 +1,17 @@
 <?php 
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
+if (isset($POST['submit'])) {
+    $name = $_POST["name"];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
-    $email_form = 'michellemarano11@gmail.com';
+    $mailTo = "mmarano@flemingcollege.ca";
+    $headers = "From: ".$mailFrom;
 
-    $email_subject = "New Form Submission";
 
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
-    $to = "mmarano@flemingcollege.ca";
-
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
-    mail($to,$email_subject,$email_body,$headers);
-    header("Location: index.html")
+    mail($mailTo, $subject, $headers);
+    header("Location: index.php?mailsend");
+}
 
 
     ?>
